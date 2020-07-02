b85f930955df15b6b80656bbad69c52961b4b07b
Kamagate-6423 <kelm6423@gmail.com>
2020-07-02 23:05:17 +0100

    Ajout de la date de deconnexion du client

--- controller/detruireSession.php
+++ controller/detruireSession.php
@@ -1,13 +1,21 @@
 <?php
 session_start();
 
+include('../administration/bdd.php');
+$bdd=new BDD();
 
 if(isset($_GET['ok'])){ // Lorsqu'une personne valide sa commande 
 	unset($_SESSION['panier']);
 	unset($_SESSION['commander']);
 	unset($_SESSION['compt']);
+	
 }else{ // Lorsqu'une personne se deconnecte
 	session_destroy();
+	
+	$idCli=$_POST['idCli'];
+
+	$date_con='UPDATE clients SET date_connexion = NOW() WHERE id_cli=:id';
+		$bdd->requetes($date_con,array('id'=>$idCli));
 }
 
 header("Location:../index.php");
